30dcfb129fe11eded454bb0e517c97a32b3016ed
Maibritt Hønholt Christensen <maibritt88@hotmail.com>
2015-11-18 13:58:46 +0100

    Oversigt over biler (home) samt oversigt over medarbejdere (emploeeLogin) + tomt view, som skal bruges til at registrere bil afleveret/chekket

--- GLSOverview/GLSOverviewWeb/Controllers/HomeController.cs
+++ GLSOverview/GLSOverviewWeb/Controllers/HomeController.cs
@@ -5,24 +5,58 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Web.UI.WebControls;
 
 
 namespace GLSOverviewWeb.Controllers
 {
 
-    public enum StatusTypes { Home = 0, Out = 1, Ready = 2 }
+    public enum StatusTypes { Home = 0, Out = 1, Delivered = 2 }
     public class HomeController : Controller
     {
-        private glsoverviewdbEntities glsDb = new glsoverviewdbEntities();
-        
+        private glsoverviewdbEntities1 glsDb = new glsoverviewdbEntities1();
+
+        private car selectedCar = null;
+        private employee selecedEmployee = null;
+        private registration newRegistration = null;
+
         // GET: Home
         public ActionResult Index()
         {
             using (glsDb)
+
             {
-                var cars = glsDb.employee.ToList();
+                
+                
+                
+                var cars = glsDb.car.ToList();
                 return View(cars);
             }
+        }
+
+        public ActionResult EmployeesLogin(int id)
+        {
+            selectedCar = glsDb.car.Find(id);
+
+            using (glsDb)
+            {
+                List<employee> resList = new List<employee>();
+                var query = from e in glsDb.employee
+                            orderby e.Name ascending 
+                            select e;
+                foreach (var emp in query)
+                {
+                    resList.Add(emp);
+                }
+                ViewBag.Car = selectedCar;
+                return View(resList);
+            }
+        }
+
+        public ActionResult RegisterCarCheked(int id)
+        {
+            selecedEmployee = glsDb.employee.Find(id);
+            return View();
         }
     }
 }
