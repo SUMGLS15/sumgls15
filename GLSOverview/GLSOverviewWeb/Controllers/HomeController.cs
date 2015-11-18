e6648039a2753da9fc530c009d8eab1e6bf37ea0
Maibritt Hønholt Christensen <maibritt88@hotmail.com>
2015-11-18 11:03:43 +0100

    Index forsøg

--- GLSOverview/GLSOverviewWeb/Controllers/HomeController.cs
+++ GLSOverview/GLSOverviewWeb/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
 
 namespace GLSOverviewWeb.Controllers
 {
+
+    public enum StatusTypes { Home = 0, Out = 1, Ready = 2 }
     public class HomeController : Controller
     {
+        private glsoverviewdbEntities glsDb = new glsoverviewdbEntities();
+        
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            using (glsDb)
+            {
+                var cars = glsDb.employee.ToList();
+                return View(cars);
+            }
         }
     }
 }
