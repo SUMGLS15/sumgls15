3a287b21bdea3ac8b20f20d1f06052248e4241ad
SvendK <git@svendk.dk>
2015-11-18 11:11:36 +0100

    Ændringer til SQL-filen, inden update-database i EF.

--- glsoverviewdb.sql
+++ glsoverviewdb.sql
@@ -1,28 +1,32 @@
-USE glsoverviewdb;
+﻿USE glsoverviewdb;
 DROP TABLE IF EXISTS `Registration`;
-DROP TABLE IF EXISTS `car`;
-DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `Car`;
+DROP TABLE IF EXISTS `Employee`;
+
+-- default is for some reason offset=2, increment=10
+SET @@auto_increment_offset=1;
+SET @@auto_increment_increment=1;
 
 -- -----------------------------------------------------
--- Table `car`
+-- Table `Car`
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `Car` (
-  `CarID` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `Licenseplate` VARCHAR(20) NOT NULL,
-  `RouteNr` VARCHAR(45) NULL,
+  `RouteNo` VARCHAR(45) NULL,
   `Hauler` VARCHAR(45) NULL,
   `Status` INT NOT NULL
 );
 
 -- -----------------------------------------------------
--- Table `employee`
+-- Table `Employee`
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `Employee` (
-  `EmployeeID` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `EmployeeNumber` VARCHAR(10) NOT NULL,
-  `EmployeeName` VARCHAR(45) NOT NULL,
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `EmpNo` VARCHAR(10) NOT NULL,
+  `Name` VARCHAR(45) NOT NULL,
   `Password` varchar(20),
   `Admin` TINYINT(1) NULL DEFAULT 0
 );
@@ -32,24 +36,33 @@ CREATE TABLE IF NOT EXISTS `Employee` (
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `Registration` (
-  `RegistraionID` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `RegistrationDate` DATETIME NULL,
-  `RegistrationComment` VARCHAR(255),
-  `Registration_carID` INT NOT NULL, FOREIGN KEY (Registration_carID) REFERENCES Car(carID),
-  `Registration_employeeID` INT NOT NULL, FOREIGN KEY (Registration_employeeID) REFERENCES Employee(employeeID)
+  `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `Date` DATETIME NULL,
+  `Comment` VARCHAR(255),
+  `CarId` INT NOT NULL, FOREIGN KEY (CarId) REFERENCES Car(Id),
+  `EmployeeId` INT NOT NULL, FOREIGN KEY (EmployeeId) REFERENCES Employee(Id)
 );
 
 -- -----------------------------------------------------
 -- DEMO DATA
 -- -----------------------------------------------------
 
-INSERT INTO Car (Licenseplate, RouteNr, Hauler, Status) VALUES ("AA-12-123", "8000", "Ø", 0);
-INSERT INTO Car (Licenseplate, RouteNr, Hauler, Status) VALUES ("BB-12-123", "9000", "M", 1);
-INSERT INTO Car (Licenseplate, RouteNr, Hauler, Status) VALUES ("CC-12-123", "8240", "D", 2);
-INSERT INTO Car (Licenseplate, RouteNr, Hauler, Status) VALUES ("DD-12-123", "8210", "Ø", 0);
-INSERT INTO Car (Licenseplate, RouteNr, Hauler, Status) VALUES ("EE-12-123", "8800", "Ø", 1);
+INSERT INTO Car (Licenseplate, RouteNo, Hauler, Status) 
+	VALUES ("AA12123", "8000", "Ø", 0);
+INSERT INTO Car (Licenseplate, RouteNo, Hauler, Status) 
+	VALUES ("BB12123", "9000", "M", 1);
+INSERT INTO Car (Licenseplate, RouteNo, Hauler, Status) 
+	VALUES ("CC12123", "8240", "D", 2);
+INSERT INTO Car (Licenseplate, RouteNo, Hauler, Status) 
+	VALUES ("DD12123", "8210", "Ø", 0);
+INSERT INTO Car (Licenseplate, RouteNo, Hauler, Status) 
+	VALUES ("EE12123", "8800", "Ø", 1);
 
-INSERT INTO Employee (EmployeeNumber, EmployeeName, Password, Admin) VALUES ("123", "Leo Møller", "321", 1);
-INSERT INTO Employee (EmployeeNumber, EmployeeName, Password, Admin) VALUES ("234", "Morten KP", NULL, 0);
-INSERT INTO Employee (EmployeeNumber, EmployeeName, Password, Admin) VALUES ("987", "SvendDK", NULL, 0);
-INSERT INTO Employee (EmployeeNumber, EmployeeName, Password, Admin) VALUES ("567", "Kim de Vos", NULL, 0);
+INSERT INTO Employee (EmpNo, Name, Password, Admin) 
+	VALUES ("123", "Leo Møller", "321", 1);
+INSERT INTO Employee (EmpNo, Name, Password, Admin) 
+	VALUES ("234", "Morten KP", NULL, 0);
+INSERT INTO Employee (EmpNo, Name, Password, Admin) 
+	VALUES ("987", "SvendDK", NULL, 0);
+INSERT INTO Employee (EmpNo, Name, Password, Admin)  
+	VALUES ("567", "Kim de Vos", NULL, 0);
