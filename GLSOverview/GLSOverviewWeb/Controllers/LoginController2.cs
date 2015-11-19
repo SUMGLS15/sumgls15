d86b5270251f946525084de195192a530e755683
SvendK <git@svendk.dk>
2015-11-19 22:50:38 +0100

    Moved Model files into /Models/

--- GLSOverview/GLSOverviewWeb/Controllers/LoginController2.cs
+++ GLSOverview/GLSOverviewWeb/Controllers/LoginController2.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GLSOverviewWeb.Models;
 
 namespace GLSOverviewWeb.Controllers
 {
-    public class LoginController : Controller
+    public class LoginController2 : Controller
     {
         private glsoverviewdbEntities db = new glsoverviewdbEntities();
         // GET: Admin
--- GLSOverview/GLSOverviewWeb/Models/Model1.Context.cs
+++ GLSOverview/GLSOverviewWeb/Models/Model1.Context.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GLSOverviewWeb
+namespace GLSOverviewWeb.Models
 {
     using System;
     using System.Data.Entity;
