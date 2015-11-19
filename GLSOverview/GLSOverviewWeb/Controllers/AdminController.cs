8b790db591c1b4366a37e330566fe5c1fbcfec0e
Morten Kjær Pedersen <eaamoped17@students.eaaa.dk>
2015-11-19 09:12:38 +0100

    Added Admincontroller

--- GLSOverview/GLSOverviewWeb/Controllers/AdminController.cs
+++ GLSOverview/GLSOverviewWeb/Controllers/AdminController.cs
@@ -8,32 +8,32 @@ namespace GLSOverviewWeb.Controllers
 {
     public class AdminController : Controller
     {
-        glsoverviewdbEntities1 db = new glsoverviewdbEntities1();
+        private glsoverviewdbEntities1 db = new glsoverviewdbEntities1();
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Index(employee emp) {
-        //    try {
-        //        Session["UserAdmin"] = emp.EmpNo;
-        //    }
-        //    catch (Exception e) {
-        //        Console.WriteLine(e);
-        //    }
+        [HttpPost]
+        public ActionResult Index(employee emp) {
+            try {
+                Session["UserAdmin"] = emp.EmpNo;
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
 
-        //    string userSession = (string)Session["UserAdmin"];
-        //    employee login = (employee)(from e in db.employee
-        //                              where e.EmpNo == emp.EmpNo
-        //                              select e).FirstOrDefault();
-        //    if (login != null && login.Admin == true) {
-        //        return );
-        //    }
-        //    else {
-        //        return View();
-        //    }
-        //}
+            string userSession = (string)Session["UserAdmin"];
+            employee login = (employee)(from e in db.employee
+                                        where e.EmpNo == emp.EmpNo
+                                        select e).FirstOrDefault();
+            if (login != null && login.Admin == true) {
+                
+            }
+            else {
+                return View();
+            }
+        }
     }
 }
