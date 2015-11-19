97675e1cfdaf9e5705a4a1b787fddd5eafcc67b1
SvendK <git@svendk.dk>
2015-11-20 00:53:12 +0100

    Tried creating the login system, but it is not done by a long shot :-(

--- GLSOverview/GLSOverviewWeb/Models/Model1.Designer.cs
+++ GLSOverview/GLSOverviewWeb/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Svend\Dropbox\Datamatiker\__Kode\_VisualStudio\151102_SUM_Projekt_GLS\GLSOverview\GLSOverviewWeb\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'G:\Dropbox\Datamatiker\__Kode\_VisualStudio\151102_SUM_Projekt_GLS\GLSOverview\GLSOverviewWeb\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
