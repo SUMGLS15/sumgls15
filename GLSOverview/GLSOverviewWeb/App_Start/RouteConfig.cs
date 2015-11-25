b238f410f06fd50397c7bddb68ccb387e527ec5e
SvendK <git@svendk.dk>
2015-11-25 21:15:53 +0100

    Added Leos hashed password to db-script. Removed the routeconfig again until I have time to actually implement the move of /admin/

--- GLSOverview/GLSOverviewWeb/App_Start/RouteConfig.cs
+++ GLSOverview/GLSOverviewWeb/App_Start/RouteConfig.cs
@@ -18,12 +18,6 @@ namespace GLSOverviewWeb
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            routes.MapRoute(
-    name: "Admin",
-    url: "Admin/{controller}/{action}/{id}",
-    defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
-);
         }
     }
 }
