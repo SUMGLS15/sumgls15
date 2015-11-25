84234999dc52456fae24e2d398b38949cf37568a
Morten Kjær Pedersen <eaamoped17@students.eaaa.dk>
2015-11-25 14:31:11 +0100

    Registeringer med kommentarer for admin oprettet. Mangler visninger af kommentarer.

---
+++ GLSOverview/GLSOverviewWeb/Viewmodels/AdminModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GLSOverviewWeb.Models;
+
+namespace GLSOverviewWeb.Viewmodels {
+    public class AdminModel {
+
+        public int EmployeeID { get; set; }
+        public int CarID { get; set; }
+        public employee Employee { get; set; }
+        public car Car { get; set; }
+        public int RegID { get; set; }
+        public registration Registration { get; set; }
+
+        public virtual List<registration> RegistrationList { get; set; }
+    }
+}
