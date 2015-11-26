2b099290a92f488c184a52392324e66ad190f352
SvendK <git@svendk.dk>
2015-11-27 00:22:44 +0100

    Fixed register parking nullpointer. Refactored the whole register parking process. Simplified RegistrationModel

--- GLSOverview/GLSOverviewWeb/Controllers/HomeController.cs
+++ GLSOverview/GLSOverviewWeb/Controllers/HomeController.cs
@@ -9,88 +9,114 @@ using System.Web.UI.WebControls;
 using GLSOverviewWeb.Models;
 using GLSOverviewWeb.Viewmodels;
 
-namespace GLSOverviewWeb.Controllers {
-    public enum StatusTypes {
+namespace GLSOverviewWeb.Controllers
+{
+    public enum StatusTypes
+    {
         Departed, Arrived, Parked
     }
 
-    public class HomeController :Controller {
+    public class HomeController : Controller
+    {
         private glsoverviewdbEntities _db = new glsoverviewdbEntities();
 
         [HttpGet]
-        public ActionResult Index() {
-            var RM = new RegistrationModel();
-
-            RM.Cars = _db.cars.ToList();
-            return View(RM);
+        public ActionResult Index()
+        {
+            return View(_db.cars.ToList());
         }
 
 
-        // TODO SK Fjernes, men dele skal bruges i adminregistrations
+        //// TODO SK Fjernes, men dele skal bruges i adminregistrations
+        //[HttpGet]
+        //public ActionResult IndexArchive(DateTime? showDate)
+        //{
+        //    if (!LoginController.IsAdmin())
+        //        return View("~/Views/Login/Index.cshtml");
+
+        //    if (showDate == null)
+        //        return View("Index");
+
+        //    var RM = new RegistrationModel();
+        //    RM.Cars = _db.cars.ToList();
+
+        //    foreach (var car in RM.Cars)
+        //    {
+        //        car.Status = (int)StatusTypes.Arrived;
+        //    }
+
+        //    var archiveDateStart = showDate.Value.Date;
+        //    var archiveDateEnd = showDate.Value.Date.AddDays(1).AddSeconds(-1);
+
+        //    var registrations = _db.registrations.Where(r =>
+        //        r.Date >= archiveDateStart &&
+        //        r.Date <= archiveDateEnd
+        //        ).ToList();
+
+        //    foreach (var registration in registrations)
+        //    {
+        //        var car = RM.Cars.FirstOrDefault(c => c.Id == registration.CarId);
+        //        if (car != null)
+        //            car.Status = (int)StatusTypes.Parked;
+        //    }
+
+        //    return View("Index", RM);
+        //}
+
         [HttpGet]
-        public ActionResult IndexArchive(DateTime? showDate) {
-            if (!LoginController.IsAdmin())
-                return View("~/Views/Login/Index.cshtml");
+        public ActionResult EmployeesLogin(int? carId)
+        {
+            if (carId == null)
+                return RedirectToAction("Index");
 
-            if (showDate == null)
-                return View("Index");
+            using (_db)
+            {
+                var employees = (from e in _db.employees
+                                 from r in _db.registrations.Where(x => x.EmployeeId == e.Id).DefaultIfEmpty()
+                                 orderby r.Date descending
+                                 select e).ToList();
 
-            var RM = new RegistrationModel();
-            RM.Cars = _db.cars.ToList();
-
-            foreach (var car in RM.Cars) {
-                car.Status = (int)StatusTypes.Arrived;
-            }
-
-            var archiveDateStart = showDate.Value.Date;
-            var archiveDateEnd = showDate.Value.Date.AddDays(1).AddSeconds(-1);
-
-            var registrations = _db.registrations.Where(r =>
-                r.Date >= archiveDateStart &&
-                r.Date <= archiveDateEnd
-                ).ToList();
-
-            foreach (var registration in registrations) {
-                var car = RM.Cars.FirstOrDefault(c => c.Id == registration.CarId);
-                if (car != null)
-                    car.Status = (int)StatusTypes.Parked;
-            }
-
-            return View("Index", RM);
-        }
-
-        [HttpGet]
-        public ActionResult EmployeesLogin(RegistrationModel rm) {
-            using (_db) {
-                var top5 = (from e in _db.employees
-                            from r in _db.registrations.Where(x => x.EmployeeId == e.Id).DefaultIfEmpty()
-                            orderby r.Date descending
-                            select e).ToList();
-
-                rm.Employees = top5;
-                rm.Car = _db.cars.Find(rm.CarID);
+                var rm = new RegistrationModel
+                {
+                    Car = _db.cars.Find(carId),
+                    Employees = employees
+                };
+                
                 return View(rm);
             }
         }
 
         [HttpGet]
-        public ActionResult RegisterCarChecked(RegistrationModel rm) {
-            rm.Car = _db.cars.Find(rm.CarID);
-            rm.Employee = _db.employees.Find(rm.EmployeeID);
-            return View(rm);
+        public ActionResult RegisterCarChecked(int? carId, int? empId)
+        {
+            if (carId == null || empId == null)
+                return RedirectToAction("Index");
+
+            using (_db)
+            {
+                var rm = new RegistrationModel
+                {
+                    Car = _db.cars.Find(carId),
+                    Employee = _db.employees.Find(empId)
+                };
+                return View(rm);
+            }
         }
 
-        [HttpPost, ActionName("RegisterCarChecked")]
-        public ActionResult PostRegisterCarChecked(RegistrationModel rm) {
-            registration reg = new registration();
-            reg.CarId = rm.CarID;
-            reg.EmployeeId = rm.EmployeeID;
-            reg.Date = DateTime.Now;
-            reg.Comment = rm.Comment;
-            reg.Position = rm.Car.Position;
+        [HttpPost]
+        public ActionResult RegisterCarChecked(RegistrationModel rm)
+        {
+            var reg = new registration
+            {
+                CarId = rm.Car.Id,
+                EmployeeId = rm.Employee.Id,
+                Date = DateTime.Now,
+                Comment = rm.Comment,
+                Position = rm.Car.Position
+            };
             _db.registrations.Add(reg);
 
-            car car = _db.cars.Find(rm.CarID);
+            var car = _db.cars.Find(rm.Car.Id);
             car.Status = (int)StatusTypes.Parked;
 
             _db.SaveChanges();
@@ -98,17 +124,21 @@ namespace GLSOverviewWeb.Controllers {
             return RedirectToAction("Index", "Home");
         }
 
-        public static void ResetCars() {
-            using (var _db = new glsoverviewdbEntities()) {
-                DateTime lastAddedReg = _db.registrations.Max(r => r.Date);
+        // TODO Fix: Is run on EVERY page view, until a car is parked on that day?
+        public static void ResetCars()
+        {
+            using (var _db = new glsoverviewdbEntities())
+            {
+                var lastAddedReg = _db.registrations.Max(r => r.Date);
 
-                if (lastAddedReg.Date < DateTime.Today) {
-                    foreach (car car in _db.cars) {
-                        car.Status = (int)StatusTypes.Arrived;
-                    }
+                if (lastAddedReg.Date >= DateTime.Today) return;
 
-                    _db.SaveChanges();
+                foreach (var car in _db.cars)
+                {
+                    car.Status = (int)StatusTypes.Arrived;
                 }
+
+                _db.SaveChanges();
             }
         }
     }
--- GLSOverview/GLSOverviewWeb/Viewmodels/RegistrationModel.cs
+++ GLSOverview/GLSOverviewWeb/Viewmodels/RegistrationModel.cs
@@ -10,57 +10,21 @@ namespace GLSOverviewWeb.Viewmodels
 {
     public class RegistrationModel
     {
-
-        // === Brugt ifm. registration (altså, det der rent faktisk skal ligge i registration model)
-
-        // Brugt i Home/Index view når der klikkes på en bil
-        // Brugt i HomeController/EmployeesLogin til at gemme rm.Car 
-        // Brugt i Home/EmployeesLogin view til til at vælge bil (querystring-parameter)
-        // Brugt i HomeController/RegisterCarChecked til at gemme rm.Car (dvs. EmployeesLogin behøver ikke gemme car)
-        public int CarID { get; set; }
-
-        // Brugt i Home controller og Home/Index view
-        public IEnumerable<car> Cars { get; set; }
-
-        // Licenseplate, RouteNo og Hauler brugt i Home/Index view, men kun som overskrifter (@Html.DisplayNameFor(model => model.Licenseplate))
-        [DisplayName("License Plate: ")]
-        public string Licenseplate { get; set; }
-        [DisplayName("Route: ")]
-        public string RouteNo { get; set; }
-        public string Hauler { get; set; }
-
-        // Brugt i Home/Index view til at vise Car.Position
-        // Brugt i Home/EmployeesLogin view til at vise Car.Licenseplate
-        // Brugt i Home/RegisterCarChecked view til at vise Car.Licenseplate samt Car.Licenseplate (igen) og Car.Position
         public car Car { get; set; }
-
-        // Brugt i HomeController/RegisterCarChecked til at gemme Employee
-        public int EmployeeID { get; set; }
-
-        // Brugt i Home/RegisterCarChecked view til at vise Employee.Name
+        
         public employee Employee { get; set; }
 
-        // Gemmes i HomeController/EmployeesLogin
-        // Brugt i Home/EmployeesLogin view til at vise 2 lister over employees
-        public IEnumerable<employee> Employees { get; set; }
+        public List<employee> Employees { get; set; }
 
-        // Brugt i Home/RegisterCarChecked view til at vise tekstboks med Comment input
         public string Comment { get; set; }
 
         // Brugt i Home/RegisterCarChecked view til at vise liste over checks
-        public string[] CheckList = new string[5] {
-            "Check cabin and trunk for parcels", 
-            "Close all windows", 
-            "Park 0,5 m. from the port", 
-            "Engaged the parking brake", 
-            "Put the key at the correct spot in the safe" 
+        public string[] CheckList = {
+            "checked cabin and trunk for parcels", 
+            "closed all windows", 
+            "parked 0,5 m. from the port", 
+            "engaged the parking brake", 
+            "put the key at the correct spot in the safe" 
         };
-        
-        // === Det der ikke bliver brugt ifm. registration
-        
-        [DisplayName("Show overview for date")]
-        [DataType(DataType.DateTime)]
-        public DateTime? ShowDate { get; set; }
-
     }
 }
