88a2c78abba44e1e0e463f4c478eaa31dd197777
SvendK <git@svendk.dk>
2015-11-29 21:13:08 +0100

    Opdateret SQL scriptet med korrekte navne og biler.

--- glsoverviewdb.sql
+++ glsoverviewdb.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `Car` (
   `Licenseplate` VARCHAR(20) NOT NULL,
   `RouteNo` VARCHAR(45) NULL,
   `Hauler` VARCHAR(45) NULL,
-  `Status` INT NOT NULL,
+  `Status` INT NOT NULL DEFAULT 0,
   `Position` VARCHAR(45) NOT NULL DEFAULT "N/A"
 );
 
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `Employee` (
   `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `EmpNo` VARCHAR(10) NOT NULL,
   `Name` VARCHAR(45) NOT NULL,
-  `Password` varchar(40), -- SHA1 hashes are 40 chars
+  `Password` varchar(40) DEFAULT NULL, -- SHA1 hashes are 40 chars
   `Admin` TINYINT(1) NOT NULL DEFAULT 0
 );
 
@@ -51,28 +51,35 @@ CREATE TABLE IF NOT EXISTS `Registration` (
 -- DEMO DATA
 -- -----------------------------------------------------
 
-INSERT INTO Car (Licenseplate, RouteNo, Hauler, Status, Position) 
+INSERT INTO Car (Licenseplate, RouteNo, Hauler, Position) 
 	VALUES
-		("AA 12 123", "8000", "Ø", 0, "Port 3"),
-		("BB 12 123", "9000", "M", 1, "Port 6"),
-		("CC 12 123", "8240", "D", 2, "Port 5"),
-		("DD 12 123", "8210", "Ø", 0, "At the main gate"),
-		("EE 12 123", "8800", "Ø", 1, "By the fence"),
-		("FF 12 123", "8801", "D", 1, "By the fence");
+		("CF 96 237", "8806", "Ø", "Port 3"),
+		("AA 10 559", "8807", "M", "Port 6"),
+		("BW 97 791", "8808", "D", "Port 5"),
+		("AT 33 059", "8809", "Ø", "At the main gate"),
+		("BF 91 770", "8810", "Ø", "By the fence"),
+		("AU 72 471", "8814", "D", "By the fence"), 
+		("AA 10 840", "8815", "Ø", "Port 34"),
+		("BG 93 709", "8815", "Ø", "Port 36"),
+		("CG 93 673", "8816", "Ø", "Port 12");
 
 INSERT INTO Employee (EmpNo, Name, Password, Admin) 
 	VALUES 
-		("123", "Leo Møller", "5f6955d227a320c7f1f6c7da2a6d96a851a8118f", 1),
-		("234", "Morten KP", NULL, 0),
-		("987", "SvendK", NULL, 0),
-		("567", "Kim de Vos", NULL, 0),
-		("652", "Palle", NULL, 0),
-		("852", "Polle", NULL, 0),
-		("373", "Ruth", NULL, 0),
-		("834", "Piotr", NULL, 0);
+		("123", "Leo Møller", "5f6955d227a320c7f1f6c7da2a6d96a851a8118f", 1);
 
-INSERT INTO Registration (Comment, Date, CommentHandled, CarId, EmployeeId, Position)
+
+INSERT INTO Employee (EmpNo, Name) 
+	VALUES 
+		("78087", "Thomas Due Andersen"),
+		("78118", "Rasmus Bessing"),
+		("78102", "Morten Kjær Pedersen"),
+		("78112", "Mads Petersen"),
+		("78120", "Kristian Bilde Højlund"), 
+		("78184", "Frederik Thomsen"),
+		("78067", "Amaury Montaufier");
+
+INSERT INTO Registration (Date, CarId, Position, EmployeeId, Comment, CommentHandled)
 	VALUES
-		(NULL, '2015-11-14 12:00', 0, 3, 3, "Port 5")
+		('2015-11-14 12:12', 3, "Port 5", 3, NULL, 0),
+		('2015-11-27 16:21', 3, "Port 5", 3, "Hængsler på bilen er i stykker, hvilket gør det svært at bakke bilen til, når det blæser. Dørene blæser hele tiden i", 0);
 	
--- Test data for registringer?
